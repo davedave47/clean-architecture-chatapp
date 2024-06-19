614be881b10a32894b6f0c118abef0d17d1cdcd7
davedave47 <huynhduy2k407@gmail.com>
2024-06-19 13:50:37 +0700

    removed drizzle

--- be/drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import {defineConfig} from 'drizzle-kit';
-
-export default defineConfig({
-    schema: "./src/infras/db/config/schema.ts",
-    out: "./src/infras/db/config/migrations",
-    dialect: "postgresql"
-})
